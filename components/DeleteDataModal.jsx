8c13ac4bc171f307a05d8caeba229474961c1517
prithvish774848474 <prithvish.freelance@gmail.com>
2024-12-27 16:33:45 +0530

    feat: Added a Modal to ask for comfirmation before deleting all the data

---
+++ components/DeleteDataModal.jsx
@@ -0,0 +1,41 @@
+import "../src/tailwind.css";
+
+function DeleteDataModal({ updateShowDeleteDataModal }) {
+  return (
+    <section className="fixed top-0 left-0 bottom-0 right-0 bg-transparent backdrop-blur-md">
+      <div
+        className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] 
+        p-4 min-w-[90%] md:min-w-[40%] border-none rounded-lg bg-rose-300"
+      >
+        <p className="mb-2 text-center text-2xl font-medium font-sans text-neutral-800">
+          <span className="font-bold text-red-800">Warning!!</span> <br />
+          You will lose all the stored data and the application will load again!
+          <br />
+          <br />
+          Do you really want to delete?
+        </p>
+        <div className="mt-4 flex flex-row items-center justify-between">
+          <button
+            onClick={() => {
+              window.localStorage.removeItem("to-do-list-data");
+              window.location.reload();
+            }}
+            className="px-4 py-1 border-none outline-0 rounded-md 
+            bg-red-600 text-white text-lg font-bold"
+          >
+            Delete
+          </button>
+          <button
+            onClick={() => updateShowDeleteDataModal(false)}
+            className="px-4 py-1 border-none outline-0 rounded-md 
+            bg-emerald-600 text-white text-lg font-bold"
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export { DeleteDataModal };
